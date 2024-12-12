affaadf6c2d2b415234c4d422ecd943024863fd9
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-12-12 13:15:53 +0900

    .

--- task-prj.lib.sh
+++ task-prj.lib.sh
@@ -59,3 +59,26 @@ task_astro__dev() { # Launch the Astro development server.
     esac
   done
 }
+
+# --------------------------------------------------------------------------
+# Misc
+# --------------------------------------------------------------------------
+
+# Write the routes file for the Astro build.
+# Refer the site for `_routes.json` file. // Routing · Cloudflare Pages docs https://developers.cloudflare.com/pages/functions/routing/#create-a-_routesjson-file
+post_task_astro__build() {
+  cat <<'EOF' >dist/_routes.json
+{
+   "version": 1,
+  "include": [
+    "/var/*",
+    "/api/*"
+  ],
+  "exclude": [
+    "/*.txt",
+    "/*.html",
+    "/doc/*"
+  ]
+}
+EOF
+}
