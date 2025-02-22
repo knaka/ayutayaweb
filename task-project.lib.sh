b4849e0b4b956143845f3b4a3b273b9d1e8d9df1
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-02-22 14:32:04 +0900

    .

--- task-project.lib.sh
+++ task-project.lib.sh
@@ -12,13 +12,16 @@ test "${sourced_f1649d9-}" = true && return 0; sourced_f1649d9=true
 . ./task-dyn-env.lib.sh
 
 task_dev() {
-  local pages_dev_port="$(ip_free_port)"
-  local astro_dev_port="$(ip_free_port)"
-  set_dynamic_entry "PAGES_DEV_PORT" "$pages_dev_port"
-  set_dynamic_entry "PAGES_CONTENT_PORT" "$astro_dev_port"
-  set_dynamic_entry "ASTRO_DEV_PORT" "$astro_dev_port"
-  set_dynamic_entry "ASTRO_DYNAMIC_PORT" "$pages_dev_port"
-  add_cleanup_handler cleanup_dynamic_entries
+  if ! test -r "$PROJECT_DIR"/.env.dynamic
+  then
+    local astro_dev_port="$(ip_random_free_port)"
+    local pages_dev_port="$(ip_random_free_port)"
+    set_dynamic_entry "ASTRO_DEV_PORT" "$astro_dev_port"
+    set_dynamic_entry "ASTRO_DYNAMIC_PORT" "$pages_dev_port"
+    set_dynamic_entry "PAGES_DEV_PORT" "$pages_dev_port"
+    set_dynamic_entry "PAGES_CONTENT_PORT" "$astro_dev_port"
+    add_cleanup_handler cleanup_dynamic_entries
+  fi
   # Launch Pages dev server with the interactive dev session disabled.
   task_pages__dev --invocation-mode=background --show-interactive-dev-session=false
   # Then, launch the Astro dev server.
