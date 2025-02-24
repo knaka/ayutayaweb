b59fcf43fa654e3f7af92d1bafd3100cfa778b22
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-02-24 20:17:41 +0900

    .

--- task-project.lib.sh
+++ task-project.lib.sh
@@ -1,9 +1,7 @@
 # vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
 # shellcheck shell=sh
-"${sourced_f1649d9-false}" && return 0 || sourced_f1649d9=true
+"${sourced_1f78f6a-false}" && return 0; sourced_1f78f6a=true
 
-. ./task.sh
-. ./task-node.lib.sh
 . ./task-pages.lib.sh
   set_pages_functions_src_pattern "$PROJECT_DIR/src-pages/functions/**/*.ts"
 . ./task-pages-dev.lib.sh
@@ -20,7 +18,7 @@ task_dev() {
     "ASTRO_DYNAMIC_PORT" "$pages_dev_port" \
     "PAGES_DEV_PORT" "$pages_dev_port" \
     "PAGES_CONTENT_PORT" "$astro_dev_port" \
-    # nop
+    # NOP
   chaintrap cleanup_session_env EXIT
   # Launch Pages dev server with the interactive dev session disabled.
   task_pages__dev --invocation-mode=background --show-interactive-dev-session=false
