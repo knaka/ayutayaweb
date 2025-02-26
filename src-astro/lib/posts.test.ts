7b61ea7b4baedb14fd94e85d4b30ac41c3368d2a
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-02-26 10:12:51 +0900

    .

--- src-astro/lib/posts.test.ts
+++ src-astro/lib/posts.test.ts
@@ -1,64 +1,9 @@
 import { describe, expect, test } from 'vitest'
-import { listPosts } from './posts.js'
-import { parse } from 'marked'
-import * as fs from 'node:fs';
-import fmCjs from 'front-matter';
-import * as path from 'node:path';
-import { DateTime } from 'luxon';
+import { iteratePosts, Markdown } from './posts.js'
+import { extractDateFromPath } from './posts.js';
+
 // isaacs/node-glob: glob functionality for node.js https://github.com/isaacs/node-glob
 import { globSync } from 'glob';
-
-// This is a workaround to use front-matter in ESM
-const fm = fmCjs as unknown as typeof fmCjs.default;
-
-describe("Posts", () => {
-  test('Can list posts', async () => {
-    const posts = await listPosts('/Users/knaka/doc');
-    // console.log(posts);
-    expect(posts.length).toBeGreaterThan(0);
-  });
-});
-
-type MyAttr = {
-  title: string;
-  Title: string;
-  TITLE: string;
-
-  id: string;
-  Id: string;
-  ID: string;
-
-  created_at_rfc: string;
-  CreatedAtRfc: string;
-  CREATED_AT_RFC: string;
-};
-
-function extractDateFromPath(filePath: string): string | null {
-  const pathParts = filePath.split(path.sep);
-
-  // Search for `doc` in the path
-  const docIndex = pathParts.findIndex(part => part === "doc");
-  if (docIndex === -1 || docIndex + 2 >= pathParts.length) {
-    return null;
-  }
-
-  // The next part should be a year
-  const year = pathParts[docIndex + 1];
-  if (!/^\d{4}$/.test(year)) {
-    return null;
-  }
-
-  // The next part should be a date
-  const monthDayPart = pathParts[docIndex + 2];
-  const monthDayMatch = monthDayPart.match(/^(\d{2})(\d{2})/);
-  if (! monthDayMatch) {
-    return null;
-  }
-
-  // Then, return the date in ISO format
-  const [, month, day] = monthDayMatch;
-  return `${year}-${month}-${day}`;
-}
 
 describe("Date extraction", () => {
   test('Can extract date from path', async () => {
@@ -67,41 +12,6 @@ describe("Date extraction", () => {
     expect(date).toBe('2015-01-19');
   });
 });
-
-class Markdown {
-  title?: string;
-  idOriginal?: string;
-  id?: string;
-  createdAt: DateTime;
-  body: string;
-  constructor(filePath?: string, zone?: string) {
-    const content = fs.readFileSync(filePath, 'utf-8');
-    const fmResult = fm<MyAttr>(content);
-    const attrs = fmResult.attributes;
-    this.title = attrs.title || attrs.Title || attrs.TITLE;
-    this.idOriginal = attrs.id || attrs.Id || attrs.ID;
-    if (this.idOriginal) {
-      this.id = this.idOriginal.slice(0, 7);
-    }
-    const createdAtRfc = attrs.created_at_rfc || attrs.CreatedAtRfc || attrs.CREATED_AT_RFC;
-    if (createdAtRfc) {
-      this.createdAt = DateTime.fromISO(createdAtRfc);
-    } else {
-      if (filePath) {
-        const date = extractDateFromPath(filePath);
-        if (date) {
-          this.createdAt = DateTime.fromISO(date, { zone: zone ?? 'Asia/Tokyo' });
-        } else {
-          this.createdAt = DateTime.local();
-        }
-      }
-    }
-    this.body = fmResult.body;
-  };
-  bodyHtml() {
-    return parse(this.body);
-  };
-}
 
 describe("Markdown", () => {
   test('Can parse markdown', async () => {
@@ -115,5 +25,28 @@ describe("Markdown", () => {
     expect(md.idOriginal).toBe('b18134d9d11b51da4e2e');
     expect(md.createdAt.toISO()).toBe('2015-10-19T00:00:00.000+09:00');
     expect(md.bodyHtml()).toContain('<p>その過程で目にした');
+    expect(md.public).toBe(true);
+  });
+
+  test('Private markdown', async () => {
+    const md = new Markdown('/Users/knaka/doc/2015/0311qiita-a4cd14b-Vagrantでupされた環境の情報はどこに保存される？.md');
+    // const md = new Markdown('/Users/knaka/doc/2021/0908qiita-2f94507-Goで、デバッガのattachまで実行を待機させる.md');
+    expect(md.public).toBe(true);
+  });
+
+  test('Markdown without public nor private', async () => {
+    const md = new Markdown('/Users/knaka/doc/2021/1000.md');
+    expect(md.public).toBe(false);
+  });
+});
+
+describe("Posts", () => {
+  test('Can list posts', async () => {
+    let count = 0;
+    for await (const _md of iteratePosts('/Users/knaka/doc/2021')) {
+      count++;
+    }
+    console.log(`count: ${count}`);
+    expect(count).toBeGreaterThan(0);
   });
 });
--- src-astro/lib/posts.ts
+++ src-astro/lib/posts.ts
@@ -1,38 +1,132 @@
-import { Dirent, promises as fs } from "node:fs";
-import * as path from "node:path";
+import { readFileSync, Dirent, promises as fs } from "node:fs";
+import { parse } from 'marked';
+// import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { DateTime } from 'luxon';
 
-type WalkDirFunc = (path: string, entry: Dirent) => Promise<void>;
+// This is a workaround to use front-matter in ESM
+import fmCjs from 'front-matter';
+const fm = fmCjs as unknown as typeof fmCjs.default;
 
-async function walkDir(dir: string, fn: WalkDirFunc): Promise<void> {
-  try {
-    const entries = await fs.readdir(dir, { withFileTypes: true });
-    for (const entry of entries) {
-      const entryPath = path.join(dir, entry.name);
-      if (entry.isDirectory()) {
-        await walkDir(entryPath, fn);
-      } else {
-        await fn(entryPath, entry);
+async function listFilePathsRec(dir: string): Promise<string[]> {
+  const paths: string[] = [];
+  for (const entry of await fs.readdir(dir, { withFileTypes: true })) {
+    const entryPath = path.join(dir, entry.name);
+    if (entry.isDirectory()) {
+      paths.push(...await listFilePathsRec(entryPath));
+    } else {
+      paths.push(entryPath);
+    }
+  }
+  return paths;
+}
+
+export async function* iteratePosts(...docDirPaths: string[]): AsyncGenerator<Markdown> {
+  for (const docDirPath of docDirPaths) {
+    for await (const filePath of await listFilePathsRec(docDirPath)) {
+      if (path.extname(filePath) !== ".md") {
+        continue;
+      }
+      const md = new Markdown(filePath);
+      if (! md.public) {
+        continue;
+      }
+      yield md;
+    }
+  }
+}
+
+type Attributes = {
+  title: string;
+  Title: string;
+  TITLE: string;
+
+  id: string;
+  Id: string;
+  ID: string;
+
+  created_at_rfc: string;
+  CreatedAtRfc: string;
+  CREATED_AT_RFC: string;
+
+  private: boolean;
+  Private: boolean;
+  PRIVATE: boolean;
+
+  public: boolean;
+  Public: boolean;
+  PUBLIC: boolean;
+};
+
+export function extractDateFromPath(filePath: string): string | null {
+  const pathParts = filePath.split(path.sep);
+
+  // Search for `doc` in the path
+  const docIndex = pathParts.findIndex(part => part === "doc");
+  if (docIndex === -1 || docIndex + 2 >= pathParts.length) {
+    return null;
+  }
+
+  // The next part should be a year
+  const year = pathParts[docIndex + 1];
+  if (!/^\d{4}$/.test(year)) {
+    return null;
+  }
+
+  // The next part should be a date
+  const monthDayPart = pathParts[docIndex + 2];
+  const monthDayMatch = monthDayPart.match(/^(\d{2})(\d{2})/);
+  if (! monthDayMatch) {
+    return null;
+  }
+
+  // Then, return the date in ISO format
+  const [, month, day] = monthDayMatch;
+  return `${year}-${month}-${day}`;
+}
+
+export class Markdown {
+  title?: string;
+  idOriginal?: string;
+  id?: string;
+  public: boolean;
+  createdAt: DateTime;
+  body: string;
+  constructor(filePath?: string, zone?: string) {
+    const content = readFileSync(filePath, 'utf-8');
+    const fmResult = fm<Attributes>(content);
+    const attrs = fmResult.attributes;
+    this.title = attrs.title || attrs.Title || attrs.TITLE;
+    this.idOriginal = attrs.id || attrs.Id || attrs.ID;
+    if (
+      attrs.private == undefined && attrs.Private == undefined && attrs.PRIVATE == undefined &&
+      attrs.public == undefined && attrs.Public == undefined && attrs.PUBLIC == undefined
+    ) {
+      this.public = false;
+    } else if (attrs.public || attrs.Public || attrs.PUBLIC) {
+      this.public = true;
+    } else if (! attrs.private || ! attrs.Private || ! attrs.PRIVATE) {
+      this.public = true;
+    }
+    if (this.idOriginal) {
+      this.id = this.idOriginal.slice(0, 7);
+    }
+    const createdAtRfc = attrs.created_at_rfc || attrs.CreatedAtRfc || attrs.CREATED_AT_RFC;
+    if (createdAtRfc) {
+      this.createdAt = DateTime.fromISO(createdAtRfc);
+    } else {
+      if (filePath) {
+        const date = extractDateFromPath(filePath);
+        if (date) {
+          this.createdAt = DateTime.fromISO(date, { zone: zone ?? 'Asia/Tokyo' });
+        } else {
+          this.createdAt = DateTime.local();
+        }
       }
     }
-  } catch (error) {
-    console.error(`Error reading directory ${dir}:`, error);
-  }
+    this.body = fmResult.body;
+  };
+  bodyHtml() {
+    return parse(this.body);
+  };
 }
-
-let posts: string[] = [];
-
-async function treatFile(pathEntry: string, entry: Dirent): Promise<void> {
-  if (entry.isDirectory()) {
-    return;
-  }
-  if (path.extname(pathEntry) !== ".md") {
-    return
-  }
-  posts.push(pathEntry);
-}
-
-export async function listPosts(docDirPath: string): Promise<string[]> {
-  // Walk the directory and return a list of post slugs
-  await walkDir(docDirPath, treatFile);
-  return posts;
-};
