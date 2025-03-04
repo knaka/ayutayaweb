919172a9ace5a6ab50e3c5ae46464c83d3a6e194
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-04 14:32:19 +0900

    Add pagination support for post static paths and implement dynamic post pages

--- src-astro/lib/posts.ts
+++ src-astro/lib/posts.ts
@@ -26,3 +26,29 @@ export async function getPostStaticPaths(orders: Order[]): Promise<GetStaticPath
   }
   return result;
 }
+
+import { PaginateFunction } from 'astro';
+
+export async function getPostStaticPathsPaginate({ orders, paginate, pageSize = 30 }: { orders: Order[], paginate: PaginateFunction, pageSize?: number }): Promise<GetStaticPathsResult> {
+  const store = obtainStore()
+  const result = [];
+  for (const order of orders as Order[]) {
+    const posts = await store.postsAsync({ order });
+    if (order === 'desc') {
+      result.push(paginate(posts, { pageSize }));
+    }
+    result.push(paginate(posts, { props: { order }, pageSize }));
+    const yearPosts: Map<number, Markdown[]> = new Map();
+    posts.forEach((post) => {
+      const year = post.createdAt.year;
+      if (!yearPosts.has(year)) {
+        yearPosts.set(year, []);
+      }
+      yearPosts.get(year).push(post);
+    });
+    result.push(...Array.from(yearPosts.keys()).map((year) => {
+      return paginate(yearPosts.get(year), { props: { year: year.toString() }, pageSize });
+    }));
+  }
+  return result;
+}
