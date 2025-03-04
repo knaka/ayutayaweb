d379493b4f5e5de8f98d06541c2f19535bf1635b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-04 18:22:15 +0900

    Refactor post components to use local styles and implement URL-safe tag encoding

--- src-astro/lib/posts.ts
+++ src-astro/lib/posts.ts
@@ -1,6 +1,52 @@
 import { GetStaticPathsResult } from 'astro';
 import { obtainStore, Markdown, Order } from '#src/posts.js';
 
+/**
+ * https://stackoverflow.com/a/78552144
+ *
+ * @param {ArrayBuffer} buffer
+ * @return {string}
+ */
+function toBase64(buffer: ArrayBuffer): string {
+  const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+  const byteLength = buffer.byteLength;
+  const bufferView = new Uint8Array(buffer);
+  const remainingBytesCount = byteLength % 3;
+  const mainLength = byteLength - remainingBytesCount;
+
+  let string = "";
+  let i = 0;
+  for (; i < mainLength; i += 3) {
+      const chunk = (bufferView[i] << 16) | (bufferView[i + 1] << 8) | bufferView[i + 2];
+      string += base64Chars[(chunk & 0b111111000000000000000000) >> 18];
+      string += base64Chars[(chunk & 0b000000111111000000000000) >> 12];
+      string += base64Chars[(chunk & 0b000000000000111111000000) >> 6];
+      string += base64Chars[(chunk & 0b000000000000000000111111)];
+  }
+  if (remainingBytesCount === 2) {
+      const chunk = (bufferView[i] << 16) | (bufferView[i + 1] << 8);
+      string += base64Chars[(chunk & 0b111111000000000000000000) >> 18];
+      string += base64Chars[(chunk & 0b000000111111000000000000) >> 12];
+      string += base64Chars[(chunk & 0b000000000000111111000000) >> 6];
+      string += "=";
+  } else if (remainingBytesCount === 1) {
+      const chunk = (bufferView[i] << 16);
+      string += base64Chars[(chunk & 0b111111000000000000000000) >> 18];
+      string += base64Chars[(chunk & 0b000000111111000000000000) >> 12];
+      string += "==";
+  }
+  return string;
+}
+
+export function toOnUrl(tag: string): string {
+  if (/^[a-zA-z0-9_]+$/.test(tag)) {
+    return tag.toLowerCase();
+  }
+  const byteArray = new TextEncoder().encode(tag);
+  return toBase64(byteArray.buffer as ArrayBuffer);
+}
+
 export async function getPostStaticPaths(orders: Order[]): Promise<GetStaticPathsResult> {
   const store = obtainStore()
   const result: GetStaticPathsResult = [];
@@ -62,7 +108,7 @@ export async function getStaticPathsSub({
       const tagPosts: Map<string, Markdown[]> = new Map();
       posts.forEach((post) => {
         post.tags.forEach((tag) => {
-          tag = tag.toLowerCase();
+          tag = toOnUrl(tag);
           if (!tagPosts.has(tag)) {
             tagPosts.set(tag, []);
           }
