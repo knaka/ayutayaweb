e6fdb3113c00f1e0ff5c2bad730d85360fecd9a0
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-04 17:36:29 +0900

    Refactor post static paths to support pagination by year and tag, and update related components for improved structure and styling

--- src-astro/lib/posts.ts
+++ src-astro/lib/posts.ts
@@ -29,26 +29,57 @@ export async function getPostStaticPaths(orders: Order[]): Promise<GetStaticPath
 
 import { PaginateFunction } from 'astro';
 
-export async function getPostStaticPathsPaginate({ orders, paginate, pageSize = 30 }: { orders: Order[], paginate: PaginateFunction, pageSize?: number }): Promise<GetStaticPathsResult> {
+export async function getStaticPathsSub({
+  paginate,
+  param = null,
+}: {
+  paginate: PaginateFunction,
+  param?: 'year' | 'tag',
+}) {
   const store = obtainStore()
-  const result = [];
-  for (const order of orders as Order[]) {
-    const posts = await store.postsAsync({ order });
-    if (order === 'desc') {
-      result.push(paginate(posts, { pageSize }));
+  const posts = await store.postsAsync({ order: 'desc' });
+  const pageSize = 30;
+  switch (param) {
+    case 'year': {
+      const yearPosts: Map<number, Markdown[]> = new Map();
+      posts.forEach((post) => {
+        const year = post.createdAt.year;
+        if (!yearPosts.has(year)) {
+          yearPosts.set(year, []);
+        }
+        yearPosts.get(year).push(post);
+      });
+      return Array.from(yearPosts.keys()).flatMap(year =>
+        paginate(yearPosts.get(year), {
+          params: { 
+            year: year.toString(),
+          },
+          pageSize
+        })
+      );
     }
-    result.push(paginate(posts, { props: { order }, pageSize }));
-    const yearPosts: Map<number, Markdown[]> = new Map();
-    posts.forEach((post) => {
-      const year = post.createdAt.year;
-      if (!yearPosts.has(year)) {
-        yearPosts.set(year, []);
-      }
-      yearPosts.get(year).push(post);
-    });
-    result.push(...Array.from(yearPosts.keys()).map((year) => {
-      return paginate(yearPosts.get(year), { props: { year: year.toString() }, pageSize });
-    }));
+    case 'tag': {
+      const tagPosts: Map<string, Markdown[]> = new Map();
+      posts.forEach((post) => {
+        post.tags.forEach((tag) => {
+          tag = tag.toLowerCase();
+          if (!tagPosts.has(tag)) {
+            tagPosts.set(tag, []);
+          }
+          tagPosts.get(tag).push(post);
+        });
+      });
+      return Array.from(tagPosts.keys()).flatMap(tag =>
+        paginate(tagPosts.get(tag), {
+          params: {
+            tag: encodeURIComponent(tag),
+          },
+          pageSize
+        })
+      );
+    }
+    default:
+      break;
   }
-  return result;
+  return paginate(posts, { pageSize });
 }
