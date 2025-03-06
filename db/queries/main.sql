2dfeb5d53e395d2f285afae4e7d7f5628a3c17d5
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-06 23:43:33 +0900

    Add SQL queries for retrieving and updating issues; enhance issue management functionality

--- db/queries/main.sql
+++ db/queries/main.sql
@@ -13,6 +13,15 @@ INSERT INTO issues (
 SELECT *
 FROM issues
 WHERE
+  ? = issues.id
+;
+
+-- name: UpdateIssueAsync :exec
+UPDATE issues
+SET
+  title = ?,
+  description = ?,
+  updated_at = datetime('now')
+WHERE
   CAST(sqlc.narg(opt_id) AS integer) = issues.id OR
   CAST(sqlc.narg(opt_title) AS text) = issues.title
-;
