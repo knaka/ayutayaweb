0d569276a0ef3c5256b2bf127babd3f06fcad1af
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-24 11:04:48 +0900

    Refactor API route imports to use alias for improved clarity

--- app/routes/api.$.ts
+++ app/routes/api.$.ts
@@ -1,10 +1,10 @@
 import type { LoaderFunctionArgs, ActionFunctionArgs } from '@remix-run/cloudflare'
-import { app as honoApp } from '../api'
+import { app } from '#/api'
 
-export function loader(args: LoaderFunctionArgs) {
-  return honoApp.fetch(args.request)
+export function loader({ request }: LoaderFunctionArgs) {
+  return app.fetch(request)
 }
 
-export function action(args: ActionFunctionArgs) {
-  return honoApp.fetch(args.request)
+export function action({ request }: ActionFunctionArgs) {
+  return app.fetch(request)
 }
