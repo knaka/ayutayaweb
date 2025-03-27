46a5e1468e2583d4cd8e239361345c5d031602c5
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-03-28 08:24:54 +0900

    Add protected route for "Protected Foo" and enhance existing protected route structure

--- app/routes/var.protected.tsx
+++ app/routes/var.protected.tsx
@@ -1,6 +1,17 @@
+import { Outlet } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
+import React from "react";
 
 export const loader: LoaderFunction = async (args) => {
   console.log('Authentication logic here');
   return null;
 };
+
+export default (() => {
+  return (
+    <div>
+      <h1>Protected</h1>
+      <Outlet />
+    </div>
+  );
+}) as React.FC;
