d79866b6144febc7a1c06d800c2a741f97a06ffd
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-07-07 16:08:49 +0900

    feat(app): rename app dir

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  appDirectory: "rr",
+  appDirectory: "rrapp",
   ssr: true,
   future: {
     unstable_viteEnvironmentApi: true,
